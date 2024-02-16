7b15f418e8c255ac3ca2431bed457684598214a3
Pelochus <anghflores@gmail.com>
2024-02-16 18:50:02 +0100

    Added PDF and minor fix to script

--- scripts/paparazzi-gcs-install.sh
+++ scripts/paparazzi-gcs-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd paparazzi
+
 # Make Paparazzi GCS AppImage Work
 echo "# Needed for Paparazzi AppImage to work" >> /home/$USER/.bashrc
 echo "export PAPARAZZI_HOME=$(pwd)" >> /home/$USER/.bashrc
@@ -7,5 +9,5 @@ echo "export PAPARAZZI_SRC=$(pwd)" >> /home/$USER/.bashrc
 
 # Get AppImage and move to /usr/bin/pprzgcs so it can be launched by Paparazzi Center
 sudo apt install -y wget # Just in case, you should have it anyway
-sudo wget https://github.com/paparazzi/PprzGCS/releases/download/v1.0.11/pprzgcs-v1.0.11-x86_64.AppImage -O /usr/bin/pprzgcs2
-sudo chmod 755 /usr/bin/pprzgcs2
+sudo wget https://github.com/paparazzi/PprzGCS/releases/download/v1.0.11/pprzgcs-v1.0.11-x86_64.AppImage -O /usr/bin/pprzgcs
+sudo chmod 755 /usr/bin/pprzgcs
