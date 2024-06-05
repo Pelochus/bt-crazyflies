046715e25f1d0601da951efbcf57deddeea4541d
Pelochus <anghflores@gmail.com>
2024-06-05 13:14:18 +0200

    Added initial Crazyflie demo with Bitcraze firmware

---
+++ python/crazyflie_demo.py
@@ -0,0 +1,59 @@
+# Extracted from:
+# https://www.bitcraze.io/documentation/repository/crazyflie-lib-python/master/user-guides/sbs_motion_commander/
+
+import logging
+import sys
+import time
+from threading import Event
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.positioning.motion_commander import MotionCommander
+from cflib.utils import uri_helper
+
+URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
+
+DEFAULT_HEIGHT = 0.5
+
+deck_attached_event = Event()
+logging.basicConfig(level=logging.ERROR)
+
+def move_linear_simple(scf):
+    with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
+        time.sleep(1)
+        mc.forward(0.5)
+        time.sleep(1)
+        mc.turn_left(180)
+        time.sleep(1)
+        mc.forward(0.5)
+        time.sleep(1)
+
+def take_off_simple(scf):
+    with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
+        time.sleep(3)
+        mc.stop()
+
+def param_deck_flow(name, value_str):
+    value = int(value_str)
+    print(value)
+
+    if value:
+        deck_attached_event.set()
+        print('Deck is attached!')
+    else:
+        print('Deck is NOT attached!')
+
+if __name__ == '__main__':
+    cflib.crtp.init_drivers()
+
+    with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
+
+        scf.cf.param.add_update_callback(group='deck', name='bcFlow2', cb=param_deck_flow)
+        time.sleep(1)
+
+        if not deck_attached_event.wait(timeout=5):
+            print('No Flow Deck detected!')
+            sys.exit(1)
+
+        move_linear_simple(scf)
