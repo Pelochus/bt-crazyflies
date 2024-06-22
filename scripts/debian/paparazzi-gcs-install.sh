5cccd215e58c878262fdb8954f4e9a86483f9ed3
Angel-HF <49835792+Pelochus@users.noreply.github.com>
2024-06-22 20:00:17 +0200

    Update pprzgcs version

--- scripts/debian/paparazzi-gcs-install.sh
+++ scripts/debian/paparazzi-gcs-install.sh
@@ -13,5 +13,5 @@ echo "export PAPARAZZI_SRC=$(pwd)" >> /home/$USER/.bashrc
 
 # Get AppImage and move to /usr/bin/pprzgcs so it can be launched by Paparazzi Center
 sudo apt install -y wget # Just in case, you should have it anyway
-sudo wget https://github.com/paparazzi/PprzGCS/releases/download/v1.0.11/pprzgcs-v1.0.11-x86_64.AppImage -O /usr/bin/pprzgcs
+sudo wget https://github.com/paparazzi/PprzGCS/releases/download/v1.0.13/pprzgcs-v1.0.13-x86_64.AppImage -O /usr/bin/pprzgcs
 sudo chmod 755 /usr/bin/pprzgcs
